4b3941a06e450f3383a69558b9192ecff995f392
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2023-08-25 19:14:35 -0700

    Positioned img carousel arrows

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
 
 import Carousel from '../components/Carousel';
 import { carouselImages } from "@/public/images";
@@ -23,25 +26,49 @@ export default function Home() {
         <h1>
           Hello! Welcome to my website
         </h1>
-        <p>
+
+        < p >
           alskdj alskdf alsdkjf ajad jadlfk jak ljasdlf jaskd jasdk l;a jfksa jsal jksald jkal ja lkjd
         </p>
-      </div>
+      </div >
 
-      <div className="flex-center-row">
-        <div className="img-carousel">
-          {/* <Carousel /> */}
-          {carouselImages.map((img) =>
-            <Carousel image={img} />)}
+      <div className="relative-parent">
+        <button className="prev fa-icon-btn">
+          <FontAwesomeIcon icon={faAngleLeft} style={{ fontSize: 30 }} />
+        </button>
+        <button className="next fa-icon-btn">
+          <FontAwesomeIcon icon={faAngleRight} style={{ fontSize: 30 }} />
+        </button>
+
+        <div className="flex-center-row">
+          <div className="img-carousel">
+            {carouselImages.map((img) =>
+              <Carousel image={img} />)}
+          </div>
         </div>
       </div>
-
 
       <div className="flex-center-row">
         <button className="request-btn" onClick={handleClick}>
           <Link href="/request">Request Quote</Link>
         </button>
       </div>
+
+
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
+      <h1>a</h1>
 
     </>
   )
